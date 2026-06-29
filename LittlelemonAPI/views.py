8efced110d6c18813d74ad15fffd39ee8ef00faa
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-06-29 11:22:06 +0330

    ordering

--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -20,10 +20,18 @@ def menu_items(request):
         items = MenuItem.objects.select_related('category').all()
         category_name = request.query_params.get('category')
         to_price = request.query_params.get('to_price')
+        search = request.query_params.get('search')
+        ordering = request.query_params.get('ordering')
         if category_name:
             items=items.filter(category__title=category_name)
         if to_price:
             items=items.filter(price=to_price)
+        if search:
+            items = items.filter(title__contains=search)
+        if ordering:
+            ordering_fields= ordering.split(",")
+            items = items.order_by(*ordering_fields)
+    
         serialized_item = MenuItemSerializer(items,many=True)
         return Response(serialized_item.data)
     if request.method == 'POST':
