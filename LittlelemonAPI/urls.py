1eece6374210405786effec1852bbc9aa9c0ed83
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-06-29 07:03:45 +0330

    deserialization

--- LittlelemonAPI/urls.py
+++ LittlelemonAPI/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('menu-items', views.MenuItemsView.as_view()),
-    path('menu-items/<int:pk>', views.SingleMenuiItemView.as_view())
+    # path('menu-items', views.MenuItemsView.as_view()),
+    # path('menu-items/<int:pk>', views.SingleMenuiItemView.as_view()),
+    path('menu-items', views.menu_items),
+    path('menu-items/<int:pk>', views.single_item)
 ]
--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -1,11 +1,33 @@
 from rest_framework import generics
 from .models import MenuItem
 from .serializers import MenuItemSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+# class MenuItemsView(generics.ListCreateAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
 
-class MenuItemsView(generics.ListCreateAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+# class SingleMenuiItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+#     queryset= MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
 
-class SingleMenuiItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
-    queryset= MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+
+@api_view(['GET','POST'])
+def menu_items(request):
+    if request.method == 'GET':
+        items = MenuItem.objects.select_related('category').all()
+        serialized_item = MenuItemSerializer(items,many=True)
+        return Response(serialized_item.data)
+    if request.method == 'POST':
+        serialized_item = MenuItemSerializer(data=request.data)
+        serialized_item.is_valid(raise_exception=True)
+        serialized_item.save()
+        return Response(serialized_item.data,status.HTTP_201_CREATED)
+
+@api_view
+def single_item (request,id):
+    item=get_object_or_404(MenuItem,pk=id)
+    serialized_item = MenuItemSerializer(item)
+    return Response(serialized_item.data)
