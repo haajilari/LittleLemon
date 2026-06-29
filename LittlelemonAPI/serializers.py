fb741b51a3932a1b1f087a6a7fe431bcb5b789f9
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-06-29 17:24:18 +0330

    serializer update

--- LittlelemonAPI/serializers.py
+++ LittlelemonAPI/serializers.py
@@ -9,12 +9,28 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id','slug','title']
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    # stock = serializers.IntegerField(source='inventory')
-    price_after_tax = serializers.SerializerMethodField(method_name="calc_tax")
-    category = CategorySerializer
+    price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
+    category_id = serializers.IntegerField(write_only=True)
+    category = CategorySerializer(read_only=True)
     class Meta:
         model = MenuItem
-        fields = ['id','title','price','inventory','price_after_tax','category']
+        fields = ['id','title','price','stock', 'price_after_tax','category','category_id']
+        extra_kwargs = {
+                'price': {'min_value': 2},
+                'stock':{'source':'inventory', 'min_value': 0}
+                }
+    class Meta:
+        model = MenuItem
+        fields = ['id','title','price','stock', 'price_after_tax','category','category_id']
 
     def calc_tax(self,product:MenuItem):
         return  product.price * Decimal(1.1)
+    def validate_price(self, value):
+        if value < 2:
+            raise serializers.ValidationError('Price should not be less than 2.0')
+        return value  # Return value if it passes validation
+    
+    def validate_stock(self, value):
+        if value < 0:
+            raise serializers.ValidationError('Stock cannot be negative')
+        return value  # Return value if it passes validation
